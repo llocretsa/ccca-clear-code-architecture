572ed277035b3360674ffbd7edd24be8dc436c96
llocretsa <parny.paul@gmail.com>
2022-03-05 14:34:54 -0300

    test: deve simular o frete (caso de use)

---
+++ test/integration/SimulateFreight.test.ts
@@ -0,0 +1,31 @@
+import SimulateFreight from '../../src/application/useCase/simulate_freight/SimulateFreight'
+import SimulateFreightInput from '../../src/application/useCase/simulate_freight/SimulateFreightInput'
+import DefaultFreightCalculator from '../../src/domain/entity/DefaultFreightCalculator'
+import PgPromiseConnectionAdapter from '../../src/infra/database/PgPromiseConnectionAdapter'
+import ItemRepositoryDatabase from '../../src/infra/repository/database/ItemRepositoryDatabase'
+
+describe('Simular Frete', () => {
+  //
+  test('Deve simular o frete dos itens', async () => {
+    const connection = new PgPromiseConnectionAdapter()
+    const itemRepository = new ItemRepositoryDatabase(connection)
+    const freightCalculator = new DefaultFreightCalculator()
+    const simulateFreight = new SimulateFreight(itemRepository, freightCalculator)
+    const input = new SimulateFreightInput([
+      {
+        idItem: 4,
+        quantity: 1
+      },
+      {
+        idItem: 5,
+        quantity: 1
+      },
+      {
+        idItem: 6,
+        quantity: 3
+      }
+    ])
+    const output = await simulateFreight.execute(input)
+    expect(output.amount).toBe(260)
+  })
+})
