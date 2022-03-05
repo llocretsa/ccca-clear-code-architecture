df8d43a9ad687f5b442b83d668b4251d76e43120
llocretsa <parny.paul@gmail.com>
2022-03-05 14:20:10 -0300

    feat: deve criar uma conexão com o banco de dados

---
+++ src/infra/database/PgPromiseConnectionAdapter.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+import pgp from 'pg-promise'
+import Connection from './Connection'
+
+export default class PgPromiseConnectionAdapter implements Connection {
+  pgp: any
+
+  constructor() {
+    const USER = String(process.env.DB_USER)
+    const PASSWORD = String(process.env.DB_PASSWORD)
+    const HOST = String(process.env.DB_HOST)
+    const PORT = String(process.env.DB_PORT)
+    const DATA_BASE = String(process.env.DB_DATA_BASE)
+
+    this.pgp = pgp()(`postgres://${USER}:${PASSWORD}@${HOST}:${PORT}/${DATA_BASE}`)
+  }
+
+  async query(statement: string, params: any[]): Promise<any> {
+    return await this.pgp.query(statement, params)
+  }
+}
