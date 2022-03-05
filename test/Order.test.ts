5fa7e29148c4df686a334118f9ffaa240ec0a750
llocretsa <parny.paul@gmail.com>
2022-03-05 08:23:29 -0300

    test: criar um pedido com 3 itens e com frete

--- test/Order.test.ts
+++ test/Order.test.ts
@@ -35,4 +35,25 @@ describe('Criar Ordem de Compra', () => {
     const total = order.getTotal()
     expect(total).toBe(128)
   })
+
+  test('Deve criar um pedido com 3 itens com um cupom de desconto expirado', function () {
+    const cpf = '839.435.452-10'
+    const order = new Order(cpf, new Date('2022-03-01'))
+    order.addItem(new Item(1, 'Música', 'CD', 30), 3)
+    order.addItem(new Item(2, 'Vídeo', 'DVD', 50), 1)
+    order.addItem(new Item(3, 'Vídeo', 'VHS', 10), 2)
+    order.addCoupon(new Coupon('VALE20', 20, new Date('2022-02-15')))
+    const total = order.getTotal()
+    expect(total).toBe(160)
+  })
+
+  test('Deve criar um pedido com 3 itens com cálculo do frete', function () {
+    const cpf = '839.435.452-10'
+    const order = new Order(cpf)
+    order.addItem(new Item(4, "Instrumentos Musicais", "Guitarra", 1000, 100, 30, 10, 3), 1) // 30
+    order.addItem(new Item(5, "Instrumentos Musicais", "Amplificador", 5000, 100, 50, 50, 20), 1)
+    order.addItem(new Item(6, "Acessórios", "Cabo", 30, 10, 10, 10, 0.9), 3)
+    const freight = order.getFreight()
+    expect(freight).toBe(260)
+  })
 })
