2efb7dea59952c1abc474db2a64a8b3ecb44c937
llocretsa <parny.paul@gmail.com>
2022-03-05 08:21:33 -0300

    test: cupom de Desconto Válido

---
+++ test/Coupon.test.ts
@@ -0,0 +1,22 @@
+import Coupon from '../src/Coupon'
+
+describe('Cupom de Desconto Válido', () => {
+  test('Deve criar um cupom de desconto válido', () => {
+    const coupon = new Coupon('VALE20', 20, new Date('2022-03-15'))
+    const today = new Date('2022-03-01')
+    const isValid = coupon.isValid(today)
+    expect(isValid).toBeTruthy()
+  })
+  test('Deve criar um cupom de desconto expirado', () => {
+    const coupon = new Coupon('VALE20', 20, new Date('2021-12-10'))
+    const today = new Date('2022-02-15')
+    const isExpired = coupon.isExpired(today)
+    expect(isExpired).toBeTruthy()
+  })
+
+  test('Deve criar um cupom de desconto válido e calcular o desconto', () => {
+    const coupon = new Coupon('VALE20', 20)
+    const discount = coupon.calculateDiscount(1000)
+    expect(discount).toBe(200)
+  })
+})
