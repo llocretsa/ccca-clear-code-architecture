ccec59f8512c0ad50f98c190b1004d336e4cbe05
llocretsa <parny.paul@gmail.com>
2022-03-05 08:21:46 -0300

    feat: cupom de Desconto Válido

--- src/Coupon.ts
+++ src/Coupon.ts
@@ -1,7 +1,21 @@
 export default class Coupon {
-
   constructor(
     readonly code: string,
-    readonly percentage: number
+    readonly percentage: number,
+    readonly expiredDate?: Date
   ) { }
+
+  isValid(today: Date = new Date()) {
+    if (!this.expiredDate) return true
+    return this.expiredDate.getTime() >= today.getTime()
+  }
+
+  isExpired(today: Date = new Date()) {
+    return !this.isValid(today)
+  }
+
+  calculateDiscount(amount: number, today: Date = new Date()) {
+    if (this.isExpired(today)) return 0
+    return (amount * this.percentage) / 100
+  }
 }
