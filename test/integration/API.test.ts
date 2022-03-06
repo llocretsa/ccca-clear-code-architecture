2a2eb9499aa6113438ff7376f99c9f3d5da8da8f
llocretsa <parny.paul@gmail.com>
2022-03-06 09:35:52 -0300

    test: testar a API /orders (GET)

--- test/integration/API.test.ts
+++ test/integration/API.test.ts
@@ -1,6 +1,23 @@
 import axios from 'axios'
+import PlaceOrder from '../../src/application/useCase/place_order/PlaceOrder'
+import PgPromiseConnectionAdapter from '../../src/infra/database/PgPromiseConnectionAdapter'
+import DatabaseRepositoryFactory from '../../src/infra/factory/DatabaseRepositoryFactory'
+import OrderRepositoryDatabase from '../../src/infra/repository/database/OrderRepositoryDatabase'
 
 describe('API', () => {
+  let placeOrders: PlaceOrder
+  let ordersRepository: OrderRepositoryDatabase
+
+  beforeEach(() => {
+    const connection = PgPromiseConnectionAdapter.getInstance()
+    ordersRepository = new OrderRepositoryDatabase(connection)
+    const repositoryFactory = new DatabaseRepositoryFactory()
+    placeOrders = new PlaceOrder(repositoryFactory)
+  })
+
+  afterEach(async () => {
+    await ordersRepository.clear()
+  })
   test('Deve testar a API /orders (POST)', async () => {
     const response = await axios({
       url: 'http://localhost:3000/orders',
@@ -43,5 +60,25 @@ describe('API', () => {
     })
     const output = response.data
     expect(output.amount).toBe(260)
+  })
+
+  test('Deve testar a API /orders (GET)', async () => {
+    const input = {
+      cpf: '839.435.452-10',
+      orderItems: [
+        { idItem: 1, quantity: 1 },
+        { idItem: 2, quantity: 1 },
+        { idItem: 3, quantity: 3 }
+      ],
+      date: new Date('2022-03-02'),
+      coupon: 'VALE20'
+    }
+    await placeOrders.execute(input)
+    const response = await axios({
+      url: 'http://localhost:3000/orders',
+      method: 'get'
+    })
+    const orders = response.data
+    expect(orders.orders).toHaveLength(1)
   })
 })
